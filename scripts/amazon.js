d9f99eb740a3a08ca2fc19217dced2c62ba1ab58
palakDevs <palakgupta98.bioinfo@gmail.com>
2025-07-07 02:09:07 +0530

    calculate and update cart quantity

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -69,8 +69,14 @@ if(matchingItem) {
   matchingItem.quantity += 1;
 } else {cart.push({productId,
   quantity:1
-});}
+});
+}
+let cartQuantity = 0;
 
-console.log(cart)
+cart.forEach((item) => {
+cartQuantity += item.quantity;
+})
+
+document.querySelector('.js-cart-quantity').innerHTML = cartQuantity
 });
 });
